611fd13d154e321ac44ad79d7101eb42b1e492b1
Kuick An Kiat Tyler <220452p@mymail.nyp.edu.sg>
2024-11-19 12:28:43 +0800

    Define Subnet Configurations
    Define 3 subnets for Public, Private, and for Database (Private)

--- Lambda_Deployment/Infrastructure_Setup.py
+++ Lambda_Deployment/Infrastructure_Setup.py
@@ -3,7 +3,7 @@ import boto3
 
 def lambda_handler(event, context):
     #Initialise AWS Resources
-    ec2 = boto3.client('ec2')
+    ec2 = boto3.client('ec2', "us-east-1a")
 
     try: 
         # Create VPC
@@ -21,6 +21,36 @@ def lambda_handler(event, context):
         ec2.modify_vpc_attribute(VpcId=vpc_id, EnableDnsHostnames={"Value": True})
 
         # Config Subnets
+        public_sub = ec2.create_subnet(VpcId=vpc_id, CidrBlock="10.0.1.0/24", TagSpecifications=[{
+            'ResourceType':'vpc',
+            'Tags': [
+                {
+                    "Key": "Name",
+                    "Value": "Public Subnet 1"
+                }
+            ]
+        }])
+        private_sub = ec2.create_subnet(VpcId=vpc_id, CidrBlock="10.0.2.0/24", TagSpecifications=[{
+            'ResourceType':'vpc',
+            'Tags': [
+                {
+                    "Key": "Name",
+                    "Value": "Private Subnet 1"
+                }
+            ]
+        }])
+        db_sub = ec2.create_subnet(VpcId=vpc_id, CidrBlock="10.0.3.0/24", TagSpecifications=[{
+            'ResourceType':'vpc',
+            'Tags': [
+                {
+                    "Key": "Name",
+                    "Value": "Private Subnet 2 (Database)"
+                }
+            ]
+        }])
+        public_sub_id = public_sub["Subnet"]["SubnetId"]
+        private_sub_id = private_sub["Subnet"]["SubnetId"]
+        db_sub_id = db_sub["Subnet"]["SubnetId"]
 
         # Create and attach Internet Gateway
 
